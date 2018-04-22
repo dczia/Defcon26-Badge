5b903b09fb0b1ca4517edf73ae5f4775863a424d
gateherder <30308133+gateherder@users.noreply.github.com>
2018-04-22 02:37:43 -0500

    fixed pin numbers for keypad rows/cols

--- Software/dczia26_combined_test/dczia26_keypad.cpp
+++ Software/dczia26_combined_test/dczia26_keypad.cpp
@@ -26,8 +26,8 @@ char hexaKeys[ROWS][COLS] = {
     {KEY_R2C1, KEY_R2C2, KEY_R2C3},
     {KEY_R3C1, KEY_R3C2, KEY_R3C3}
 };
-byte rowPins[ROWS] = {31, 28, 27,  2}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {    25, 24, 23}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {16,  0,  2, 25}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {    12, 13, 15}; //connect to the column pinouts of the keypad
 
 
 Keypad* keypad_setup(void)
