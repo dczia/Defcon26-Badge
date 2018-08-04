a2be58d87e8390800995e0e7d19d33e9072f567e
Mat Richmond <mrichmond@xpansiv.com>
2018-08-04 15:57:04 -0600

    Update keymapping on badge
    Why:
      * Top left button on badge thinks it is bottom left
    
    How:
      * Change keypad to be on Column/Row order
      * Change LEDs to map to new ordering

--- Software/dczia26_final/dczia26_keypad.cpp
+++ Software/dczia26_final/dczia26_keypad.cpp
@@ -7,10 +7,10 @@ const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
 //define the cymbols on the buttons of the keypads
 char hexaKeys[ROWS][COLS] = {
-  {KEY_R3C3, KEY_R3C2, KEY_R3C1, KEY_R3C0},
-  {KEY_R2C3, KEY_R2C2, KEY_R2C1, KEY_R2C0},
-  {KEY_R1C3, KEY_R1C2, KEY_R1C1, KEY_R1C0},
-  {KEY_R0C3, KEY_R0C2, KEY_R0C1, KEY_R0C0}
+  {KEY_R0C0, KEY_R0C1, KEY_R0C2, KEY_R0C3},
+  {KEY_R1C0, KEY_R1C1, KEY_R1C2, KEY_R1C3},
+  {KEY_R2C0, KEY_R2C1, KEY_R2C2, KEY_R2C3},
+  {KEY_R3C0, KEY_R3C1, KEY_R3C2, KEY_R3C3}
 };
 byte rowPins[ROWS] = {17,  16,  4, 2}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {32, 33, 25, 26}; //connect to the column pinouts of the keypad
