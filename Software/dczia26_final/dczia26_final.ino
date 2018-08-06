afaafe19ac179c286a4d6d3504ff3d2d393e6b67
Lithochasm <lithochasm@gmail.com>
2018-08-05 23:03:47 -0700

    Joe cleaned up some menu code and increased text size

--- Software/dczia26_final/dczia26_final.ino
+++ Software/dczia26_final/dczia26_final.ino
@@ -1,4 +1,4 @@
-// dczia 2018 FINAL package
+// dczia 2018 proto dos test firmware
 // combines all major hardware aspects (led, oled, keypad, ble)
 
 // split into functional regions
@@ -11,10 +11,10 @@
 // Global variables
 // initilized in "setup()"
 // used in "loop()"
-Adafruit_SSD1306   *oled = NULL; 
+Adafruit_SSD1306   *oled = NULL; // uses v3.xx from "esp8266 and esp32 oled driver for ssd1306 display" (https://github.com/ThingPulse/esp8266-oled-ssd1306)
 Keypad             *keys = NULL; // currently customized and included within project (will update to forked lib later)
 
-// in arduino world, "setup()" is called once at power-up (or reset) ...
+// in arduino world, "setup()" is called once at power-up (or reset) ... 
 void setup(void)
 {
   // init system debug output
@@ -25,36 +25,44 @@ void setup(void)
   keys = keypad_setup();
   delay(1);
 
-  //Setup LEDS
-  strip.Begin();
-  strip.Show();
+    //Setup LEDS
+    strip.Begin();
+    strip.Show();
 
-  startupAnimation();
-  strip.Show();
+   startupAnimation();
+    strip.Show();
 
-  // animations.StopAnimation(0);
+   // animations.StopAnimation(0);
 
-  SetRandomSeed();
-
+    SetRandomSeed();
+    
+    
+    
+ // delay(1);
   oled = oled_setup();
+  //delay(1);
   ble_setup();
-
+  
   // call welcome screen (once)
   oled_welcome(oled);
 
   // done with init fuction
-  Serial.println("Done With Setup!");
+   Serial.println("Done With Setup!");
 
-  SetRandomSeed();
+    SetRandomSeed();
+  
 }
 
-// in arduino world, "loop()" is called over and over and over and ...
+// in arduino world, "loop()" is called over and over and over and ... 
 // you get the idea... we don't need to "while(1)" ourselves...
-<<<<<<< HEAD
 void loop(void) {
 
     // Store what mode we're currently in
     static auto mode = '1';
+
+    // Determine if we've changed the mode.  Use this to initialize
+    // the animations
+    static auto newmode = true;
 
     // Grab the current keypress.  If there is none, it will be
     // NO_KEY.
@@ -67,15 +75,19 @@ void loop(void) {
       // the new mode
       if(mode == 'D') {
           mode = keypress;
+          newmode = true;
 
       // If we're not in menu mode, but we press the menu button,
       // D, then move us into menu mode
       } else if(keypress == 'D') {
          mode = 'D';
+         newmode = true;
       }
     }
 
     // Run the code associated with the particular command
+    auto mode_name = std::string();
+    auto mode_size = 2;
     switch(mode) {
     case '1':
     case '3':
@@ -89,9 +101,11 @@ void loop(void) {
     case 'A':
     case 'B':
     case 'C':
-    case 'D':
     case '#':
-    case '*':           
+    case '*':
+        // Set the mode message
+        mode_name = "Low Rainbow";
+        
         //Default Animation Loop
         if (animations.IsAnimating()) {
             // The normal loop just needs these two to run the active animations
@@ -102,7 +116,27 @@ void loop(void) {
             FadeInFadeOutRinseRepeat(.1f); // 0.0 = black, 0.25 is normal, 0.5 is bright
         }
         break;
+    case 'D':
+        // Set the mode message
+        mode_name = "Menu";
+        mode_size = 4;
+  
+        //Default Animation Loop
+        if (animations.IsAnimating()) {
+            // The normal loop just needs these two to run the active animations
+            animations.UpdateAnimations();
+            strip.Show();
+        } else {
+            // No animation runnning, start some 
+            FadeInFadeOutRinseRepeat(0.0f); // 0.0 = black, 0.25 is normal, 0.5 is bright
+        }
+    
+        break;
+        
     case '2':
+        // Set the mode message
+        mode_name = "High Rainbow";
+    
         //Default Animation Loop
         if (animations.IsAnimating()) {
             // The normal loop just needs these two to run the active animations
@@ -114,24 +148,17 @@ void loop(void) {
         }    
         break;
     }
-=======
-void loop(void)
-{
-  auto keypress = keys->getKey(); // non-blocking
-  
-  //Default Animation Loop
-  if (animations.IsAnimating())
-  {
-    // the normal loop just needs these two to run the active animations
 
-    animations.UpdateAnimations();
-    strip.Show();
-  }
-  else
-  {
-    // no animation runnning, start some
-    FadeInFadeOutRinseRepeat(.1f); // 0.0 = black, 0.25 is normal, 0.5 is bright
-  }
->>>>>>> 5529edaea220b871a8c0fd711d32cbef043337c7
+    // Set the LED message
+    if(newmode) {
+        oled->clearDisplay();
+        oled->setCursor(0, 0);
+        oled->setTextSize(mode_size);
+        oled->println(mode_name.c_str());
+        oled->display();
+    }
+
+    // At this point, the mode has already run, so we're no longer in a new mode
+    newmode=false;
 }
 
