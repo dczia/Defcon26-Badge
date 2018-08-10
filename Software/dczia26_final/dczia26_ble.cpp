4e6d6a9921fdc0b110b0bb0e355818f04a52d88e
ozhuhem <zhuhem@gmail.com>
2018-08-09 19:13:02 -0500

    Got server client working between badge and phone

--- Software/dczia26_final/dczia26_ble.cpp
+++ Software/dczia26_final/dczia26_ble.cpp
@@ -14,11 +14,27 @@ extern "C" {
 }
 #endif
 
+Adafruit_SSD1306   *screen = NULL;
+
 BLEAdvertising *pAdvertising;
+BLEScan* pBLEScan;
+BLECharacteristic *pCharacteristic;
+BLEService *pService;
+BLEServer *pServer;
+
 struct timeval now;
+
+//may need to be in a vector later
+BLERemoteService *pRemoteService;
+BLERemoteCharacteristic *pRemoteCharacteristic;
+BLEClient *pClient;
+BLEAddress *pServerAddress;
 
 int dc26Badges;
 int dcZiaBadges;
+
+bool connected = false;
+bool doConnect = false;
 
 void setBeacon() {
 
@@ -38,7 +54,7 @@ void setBeacon() {
   strServiceData += (char)0xFF;   // Type
   strServiceData += oBeacon.getData(); 
 
-  oAdvertisementData.setName("DCZia26");
+  oAdvertisementData.setName(BADGE_NAME);
   oAdvertisementData.addData(strServiceData);
   oAdvertisementData.setAppearance(APPEARANCE_DC26);
   
@@ -51,7 +67,7 @@ void setBeacon() {
   
 }
 
-void ble_setup() {
+void ble_setup(Adafruit_SSD1306   *oled) {
   Serial.begin(115200);
   gettimeofday(&now, NULL);
 
@@ -65,19 +81,27 @@ void ble_setup() {
   BLEDevice::init("DCZia26");
 
   // Create the BLE Server
-  BLEServer *pServer = BLEDevice::createServer();
+  pServer = BLEDevice::createServer();
 
   pAdvertising = pServer->getAdvertising();
   
   setBeacon();
-  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pService = pServer->createService(SERVICE_UUID);
+
+  pCharacteristic = pService->createCharacteristic(
+                              CHARACTERISTIC_UUID_RX,
+                              BLECharacteristic::PROPERTY_READ |
+                              BLECharacteristic::PROPERTY_WRITE);
+  pCharacteristic->setValue("Come to the dark side... we have cookies");
+  pCharacteristic->addDescriptor(new BLE2902());
   pService->start();
   
   // Start advertising
   pAdvertising->start();
   Serial.println("Advertising started...");
   delay(10);
-  
+
+  screen = oled;
 }
 
 class EmptyCallback: public BLEAdvertisedDeviceCallbacks {
@@ -93,7 +117,7 @@ void ble_scan_all(Adafruit_SSD1306 *screen) {
     screen->println("Scanning all...");
     screen->display();
     delay(1);
-    BLEScan* pBLEScan = BLEDevice::getScan(); //create new scan
+    pBLEScan = BLEDevice::getScan(); //create new scan
     pBLEScan->setAdvertisedDeviceCallbacks(new EmptyCallback());
     pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
     BLEScanResults foundDevices = pBLEScan->start(BLE_SCAN_TIME);
@@ -136,6 +160,8 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
         if(advertisedDevice.getName() == "DCZia26") {
           Serial.printf("*** DCZia!!\n");
           dcZiaBadges = dcZiaBadges + 1;
+          pServerAddress = new BLEAddress(advertisedDevice.getAddress());
+          doConnect = true;
         }
       }
     }
@@ -159,3 +185,112 @@ void ble_scan_dczia(int bleScanCount[]) {
   Serial.println(dcZiaBadges);
   Serial.println("Scan done!");
 } 
+
+void ble_client() { 
+  screen->clearDisplay();
+  screen->setCursor(0, 0);
+  
+  if(!connected && doConnect)
+    connected = ble_clientConnect(pServerAddress);
+  if(connected)
+    ble_clientReport();
+
+//  //testing writting to server
+//  if(connected && !written){
+//    written = true;
+//    delay(DELAY);
+//    delay(DELAY);
+//    ble_clientWrite("The client has written!!!!");
+//  }
+  screen->display();
+}
+
+bool ble_clientConnect(BLEAddress * pAddress) {
+  screen->clearDisplay();
+  screen->setCursor(0, 0);
+  screen->println("CONNECTING!");
+
+  String tmp = String(pAddress->toString().c_str());
+  tmp = String("Server Address: " + tmp);
+  screen->println(tmp);
+  
+  Serial.print("Connecting to ");
+  Serial.println(pAddress->toString().c_str());
+
+  pClient  = BLEDevice::createClient();
+  pClient->connect(*pAddress);
+  
+  pRemoteService = pClient->getService(SERVICE_UUID);
+  if (pRemoteService == nullptr) {
+      Serial.print("Failed to find our service UUID: ");
+      Serial.println(SERVICE_UUID.toString().c_str());
+      return false;
+  }
+
+  pRemoteCharacteristic = pRemoteService->getCharacteristic(BEACON_UUID);
+  if (pRemoteCharacteristic == nullptr) {
+      Serial.print("Failed to find our characteristic UUID: ");
+      Serial.println(BEACON_UUID.toString().c_str());
+      return false;
+  }
+
+  ble_clientReport();
+  screen->display();
+  return true;
+}
+
+//prints charateristic message from another server
+void ble_clientWrite(const std::string mesg) {
+  screen->clearDisplay();
+  screen->setCursor(0, 0);
+  if(pRemoteCharacteristic->canWrite())
+  {
+    screen->println("Writing client value");
+    pRemoteCharacteristic->writeValue(mesg);
+    screen->println(mesg.c_str());
+  }
+  else
+    screen->println("Cannot write server value");
+  screen->display();
+}
+
+//prints charateristic message from another server
+void ble_clientReport() {
+  screen->clearDisplay();
+  screen->setCursor(0, 0);
+  if(pRemoteCharacteristic->canRead())
+  {
+    screen->println("Reading server value");
+    std::string value = pRemoteCharacteristic->readValue();
+    screen->println(value.c_str());
+  }
+  else
+    screen->println("Cannot read server value");
+  screen->display();
+}
+
+// prints charateristic message from this server
+void print_charateristic_msg() {
+  screen->clearDisplay();
+  screen->setCursor(0, 0);
+  String tmp = String(pCharacteristic->getValue().c_str());
+  screen->println(tmp);
+  screen->display();
+}
+
+std::string string_to_hex(const std::string& input) {
+  static const char* hex_values = "0123456789ABCDEF";
+
+  std::string output;
+  output.reserve(2*input.size()+2);
+  output.push_back('0');
+  output.push_back('x');
+  
+  for(const unsigned char c : input) 
+  {
+    output.push_back(hex_values[c >> 4]);
+    output.push_back(hex_values[c & 15]);
+  }
+  return output;
+}
+
--- Software/dczia26_final/dczia26_ble.h
+++ Software/dczia26_final/dczia26_ble.h
@@ -10,11 +10,12 @@
 #include <BLEBeacon.h>
 #include <Adafruit_SSD1306.h>
 
+#define BADGE_NAME "DCZia26_olive"
 
-#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
-#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
-#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
-#define BEACON_UUID            "8ec76ea3-6668-48da-9866-75be8bc86f4d" // UUID 1 128-Bit (may use linux tool uuidgen or random numbers via https://www.uuidgenerator.net/)
+static BLEUUID SERVICE_UUID           ("6E400001-B5A3-F393-E0A9-E50E24DCCA9E"); // UART service UUID
+static BLEUUID CHARACTERISTIC_UUID_RX ("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
+static BLEUUID CHARACTERISTIC_UUID_TX ("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
+static BLEUUID BEACON_UUID            ("8ec76ea3-6668-48da-9866-75be8bc86f4d"); // UUID 1 128-Bit (may use linux tool uuidgen or random numbers via https://www.uuidgenerator.net/)
 
 #define APPEARANCE_DC26 0x26DC
 #define APPEARANCE_DC25 0x19DC
@@ -26,6 +27,12 @@
 
 const int BLE_SCAN_TIME = 10; // 10 seconds
 
-void ble_setup(void);
+void ble_setup(Adafruit_SSD1306   *oled);
 void ble_scan_all(Adafruit_SSD1306 *screen);
 void ble_scan_dczia(int bleScanCount[]);
+void ble_client();
+bool ble_clientConnect(BLEAddress * pAddress);
+void ble_clientWrite(const std::string mesg);
+void ble_clientReport();
+void print_charateristic_msg();
+std::string string_to_hex(const std::string& input);
