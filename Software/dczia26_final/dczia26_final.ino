7f87eabf94cc2b99951fbc4ee61cdf9d0609e4eb
Aaskf <ask.wietting@gmail.com>
2019-05-27 13:26:44 -0500

    Adding latest changes before breaking everything

--- Software/dczia26_final/dczia26_final.ino
+++ Software/dczia26_final/dczia26_final.ino
@@ -12,8 +12,15 @@
 
 arduinoFFT FFT = arduinoFFT();
 
-#define COLOR_GREEN HslColor(120.0 / 360.0, 1.0, 0.5)
+#define COLOR_GREEN HslColor(120.0 / 360.0, 1.0, 0.50)
 #define COLOR_BLUE HslColor(240.0 / 360.0, 1.0, 0.05)
+#define COLOR_TEAL HslColor(175.0 / 360.0, 1.0, 0.50)
+#define COLOR_VIOLET HslColor(300.0 / 360.0, 1.0, 0.50)
+#define COLOR_RED HslColor(360.0 / 360.0, 1.0, 0.50)
+#define COLOR_YELLOW HslColor(60.0 / 360.0, 1.0, 0.50)
+#define COLOR_PINK HslColor(335.0 / 360.0, 1.0, 0.50)
+
+
 
 // Global variables
 Adafruit_SSD1306   *oled = NULL; // uses v3.xx from "esp8266 and esp32 oled driver for ssd1306 display" (https://github.com/ThingPulse/esp8266-oled-ssd1306)
@@ -33,13 +40,13 @@ void IRAM_ATTR onTimer() {
   }
 }
 
-// Set up the mic
-#define MIC1_PIN 35 // Microphone is attached to Trinket GPIO #2/Gemma D2 (A1)
-#define DC_OFFSET 0 // DC offset in mic signal - if unusure, leave 0
-#define NOISE 30 // Noise/hum/interference in mic signal
-#define SAMPLES 512 // Length of buffer for dynamic level adjustment
-#define SAMPLING_FREQUENCY 40000 // Hz, must be 40000 or less due to ADC conversion time. Determines maximum frequency that can be analysed by the FFT Fmax=sampleF/2.
-#define amplitude 50            // Depending on your audio source level, you may need to increase this value
+// Set up the mic for audio capture
+#define MIC1_PIN 35               // Microphone is attached to Trinket GPIO #2/Gemma D2 (A1)
+#define DC_OFFSET 0               // DC offset in mic signal - if unusure, leave 0
+#define NOISE 30                  // Noise/hum/interference in mic signal
+#define SAMPLES 512               // Length of buffer for dynamic level adjustment
+#define SAMPLING_FREQUENCY 40000  // Hz, must be 40000 or less due to ADC conversion time. Determines maximum frequency that can be analysed by the FFT Fmax=sampleF/2.
+#define amplitude 50             // Depending on your audio source level, you may need to increase this value
 
 unsigned int sampling_period_us;
 unsigned long microseconds;
@@ -97,7 +104,6 @@ void setup(void) {
 
   // done with init fuction
   Serial.println("Done With Setup!");
-  Serial.println(F("Idx\tMAX4466 Audio Output"));
 
 }
 
@@ -295,14 +301,9 @@ void loop(void) {
       }
       break;
     case '7':
-      // Set on audio mode!
-      // displayUpdate("Gathering audio samples");
-      // Get sound information
+      // Enable audio response mode!
       if(curIdx < datapoints){
-        //int max4466Mic = analogRead(MIC1_PIN); // Raw reading from mic
-        
-        //Serial.print(F("\t"));
-        //Serial.println(max4466Mic);
+        // Read from the mic until you hit the max captureMinutes (set above)
         oled->clearDisplay();
         for (int i = 0; i < SAMPLES; i++) {
           newTime = micros()-oldTime;
@@ -314,51 +315,62 @@ void loop(void) {
         FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
         FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
         FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+        
         oled->setTextSize(1);
         oled->setTextColor(WHITE);
         oled->setCursor(0,0);
-        oled->println(".1 .2 .5 1K 2K 4K 8K");
+        oled->println("A  .2 .5 1K 2K 4K 8K");
         for (int i = 2; i < (SAMPLES/2); i++){ // Don't use sample 0 and only first SAMPLES/2 are usable. Each array eleement represents a frequency and its value the amplitude.
-        if (vReal[i] > 2000) { // Add a crude noise filter, 10 x amplitude or more
-          if (i<=2 )             displayBand(0,(int)vReal[i]/amplitude); // 125Hz
-          if (i >3   && i<=5 )   displayBand(1,(int)vReal[i]/amplitude); // 250Hz
-          if (i >5   && i<=7 )   displayBand(2,(int)vReal[i]/amplitude); // 500Hz
-          if (i >7   && i<=15 )  displayBand(3,(int)vReal[i]/amplitude); // 1000Hz
-          if (i >15  && i<=30 )  displayBand(4,(int)vReal[i]/amplitude); // 2000Hz
-          if (i >30  && i<=53 )  displayBand(5,(int)vReal[i]/amplitude); // 4000Hz
-          if (i >53  && i<=200 ) displayBand(6,(int)vReal[i]/amplitude); // 8000Hz
-          if (i >200           ) displayBand(7,(int)vReal[i]/amplitude); // 16000Hz
-          //Serial.println(i);
-        }
-        //18*band,64-peak[band],14
+          if (vReal[i] > 2000) { // Add a crude noise filter, 10 x amplitude or more
+            // Note, these are my best guesses for frequencies tuned with a pitch pipe
+            int dsize = vReal[i]/amplitude;
+            int dmax = 50;
+            if (dsize > dmax) dsize = dmax;
+
+            if (i<=2 )             displayBand(0,(int)dsize); // 125Hz
+            if (i >3   && i<=4 )   displayBand(1,(int)dsize); // 250Hz
+            if (i >4   && i<=6 )   displayBand(2,(int)dsize); // 500Hz
+            if (i >6   && i<=14 )  displayBand(3,(int)dsize); // 1000Hz
+            if (i >14  && i<=30 )  displayBand(4,(int)dsize); // 2000Hz
+            if (i >30  && i<=53 )  displayBand(5,(int)dsize); // 4000Hz
+            if (i >53  && i<=200 ) displayBand(6,(int)dsize); // 8000Hz
+            if (i >200           ) displayBand(7,(int)dsize); // 16000Hz
+
+            //Serial.println(i);
+            //}
+          }
           for (byte band = 0; band <= 6; band++) oled->drawFastHLine(18*band,64-peak[band],14,1);
         }
         if (millis()%4 == 0) {for (byte band = 0; band <= 6; band++) {if (peak[band] > 0) peak[band] -= 1;}} // Decay the peak
-        
+
+        strip.Show();
         oled->display();
 
-          if (keys->getState('D') != HOLD) {
-  
+        // Pixel Picker!
+        if (keys->getState('D') != HOLD) {
           // Get a copy of the keymap
           uint16_t map = 0;
           for(uint8_t i = 0; i < 4; i++){
             map |= ((keys->bitMap[i] & 0x0F) << (i * 4));
           }
-  
           // Determine if a pixel needs to be on or off
           for(uint8_t i = 0; i < 16; i++){
             if((map >> i) & 0x01){
               // Key is down
-              strip.SetPixelColor(i, COLOR_GREEN);
+              if(12 <= i && i <= 15) strip.SetPixelColor(i, COLOR_GREEN);
+              if(8 <= i && i <= 11) strip.SetPixelColor(i, COLOR_TEAL);
+              if(4 <= i && i <= 7) strip.SetPixelColor(i, COLOR_YELLOW);
+              if(0 <= i && i <= 3) strip.SetPixelColor(i, COLOR_RED);
             }
             else{
-              strip.SetPixelColor(i, COLOR_BLUE);
+              if(curIdx % 30 == 0){
+                strip.SetPixelColor(i, COLOR_BLUE);
+              }
             }
           }
-          
           strip.Show();
         }
-      
+    
         curIdx++;
         delay(delayMS);
         if(curIdx >= datapoints){
@@ -460,11 +472,8 @@ void runDefaultAnimations(void){
 }
 
 void displayBand(int band, int dsize){
-  int dmax = 50;
-  if (dsize > dmax) dsize = dmax;
-  //18*6,0, 14
-  if (band == 7) oled->drawFastHLine(18*6,0,14,1);
-  //18*band,64-s, 14
+  // Draw the max number of lines for OLED EQ display
+  if (band == 7) {oled->drawFastHLine(18*6,0,14,1);}
   for (int s = 0; s <= dsize; s=s+2){oled->drawFastHLine(18*band,64-s,14,1);}
   if (dsize > peak[band]) {peak[band] = dsize;}
 }
